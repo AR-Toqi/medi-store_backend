ba07324d5d25cc9796967517f731f76a88acd981
Toqi Abdullah <toqiabdullah61990@gmail.com>
2026-05-14 00:06:13 +0600

    feat: initialize Prisma client with pg adapter and ssl connection pool

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
-import { config } from "../config";
+import { Pool } from "pg";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { config } from "../config";
 
 
 const connectionString = config.database_url;
@@ -9,7 +10,14 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is missing. Cannot initialize Prisma.");
 }
 
-const adapter = new PrismaPg({ connectionString })
+const pool = new Pool({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
 export { prisma }
