a57c6e98e522b7493fbca8b2bb8b77f7a93dcd5f
Toqi Abdullah <toqiabdullah61990@gmail.com>
2026-05-17 01:04:30 +0600

    feat: initialize and export Prisma client instance with configuration validation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,5 @@
-import { Pool } from "pg";
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 import { config } from "../config";
-
 
 const connectionString = config.database_url;
 
@@ -10,18 +7,6 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is missing. Cannot initialize Prisma.");
 }
 
-const pool = new Pool({
-    connectionString,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-pool.on('error', (err) => {
-    console.error('Unexpected error on idle client', err);
-});
-
-const adapter = new PrismaPg(pool)
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient();
 
 export { prisma }
